839c6a335a216b10a17a530ee884837e648f3975
tanmay_choudhary <tanmay.d.choudhary@gmail.com>
2022-06-09 20:43:00 +0530

    Defined two styles of cells: walls and path

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from src.generator import Generator
 
 maze_generator = Generator(10, [0, 1])
 print(maze_generator.get_maze_template())
-print("\n")
-maze_generator.gen_maze()
-print(maze_generator.get_maze_template())
+# print("\n")
+# maze_generator.gen_maze()
+# print(maze_generator.get_maze_template())
--- src/generator.py
+++ src/generator.py
@@ -1,32 +1,106 @@
 import numpy as np
-import random
+import enum
 
 
-# class MazeCell:
-#     def __init__(self, value: int, coordinates: list) -> None:
-#         self.value = value
-#         self.coordinates = np.array(coordinates)
-#         self.visited = False
+class CellMarker(enum.Enum):
+    WALL = 1
+    PATH = 0
+
+
+class Dir(enum.Enum):
+    UP = 0
+    LEFT = 1
+    DOWN = 2
+    RIGHT = 3
+
+
+class MazeCell:
+    def __init__(self, value: int, coordinates: list) -> None:
+        self.value = value
+        self.coordinates = coordinates
+
+        self.wallsList = [None] * 4
 
 
 class Generator:
     def __init__(self, side_length: int, entry_coordinates: list) -> None:
         # define the maze and entry_coordinates
-        self.maze_template = np.ones((side_length, side_length))
+        self.maze_template = np.array(
+            [MazeCell(CellMarker.WALL, [0, 0])] * side_length * side_length)
         self.__entry_coordinates = entry_coordinates
 
-        # set initial walls
-        self.maze_template[0] = [1] * side_length
-        self.maze_template[-1] = [1] * side_length
+        self.maze_template = self.maze_template.reshape(
+            (side_length, side_length))
 
-        self.maze_template[:, 0] = 1
-        self.maze_template[:, -1] = 1
+        for i in range(0, self.maze_template.shape[0]):
+            for j in range(0, self.maze_template.shape[1]):
+                self.maze_template[i, j].value = 1
+                self.maze_template[i, j].coordinates = [i, j]
+
+                if i == 0:
+                    if j == 0:
+                        self.maze_template[i, j].wallsList[Dir.DOWN] = [
+                            i + 1, j]
+                        self.maze_template[i, j].wallsList[Dir.RIGHT] = [
+                            i, j + 1]
+                    elif j == side_length - 1:
+                        self.maze_template[i, j].wallsList[Dir.DOWN] = [
+                            i + 1, j]
+                        self.maze_template[i, j].wallsList[Dir.LEFT] = [
+                            i, j - 1]
+                    else:
+                        self.maze_template[i, j].wallsList[Dir.LEFT] = [
+                            i, j - 1]
+                        self.maze_template[i, j].wallsList[Dir.DOWN] = [
+                            i + 1, j]
+                        self.maze_template[i, j].wallsList[Dir.RIGHT] = [
+                            i, j + 1]
+                if i == side_length - 1:
+                    if j == 0:
+                        self.maze_template[i, j].wallsList[Dir.UP] = [i - 1, j]
+                        self.maze_template[i, j].wallsList[Dir.RIGHT] = [
+                            i, j + 1]
+                    elif j == side_length - 1:
+                        self.maze_template[i, j].wallsList[Dir.UP] = [i - 1, j]
+                        self.maze_template[i, j].wallsList[Dir.LEFT] = [
+                            i, j - 1]
+                    else:
+                        self.maze_template[i, j].wallsList[Dir.LEFT] = [
+                            i, j - 1]
+                        self.maze_template[i, j].wallsList[Dir.UP] = [i - 1, j]
+                        self.maze_template[i, j].wallsList[Dir.RIGHT] = [
+                            i, j + 1]
+                else:
+                    if j == 0:
+                        self.maze_template[i, j].wallsList[Dir.UP] = [i - 1, j]
+                        self.maze_template[i, j].wallsList[Dir.DOWN] = [
+                            i + 1, j]
+                        self.maze_template[i, j].wallsList[Dir.RIGHT] = [
+                            i, j + 1]
+                    elif j == side_length - 1:
+                        self.maze_template[i, j].wallsList[Dir.UP] = [i - 1, j]
+                        self.maze_template[i, j].wallsList[Dir.LEFT] = [
+                            i, j - 1]
+                        self.maze_template[i, j].wallsList[Dir.DOWN] = [
+                            i + 1, j]
+                    else:
+                        self.maze_template[i, j].wallsList[Dir.UP] = [i - 1, j]
+                        self.maze_template[i, j].wallsList[Dir.LEFT] = [
+                            i, j - 1]
+                        self.maze_template[i, j].wallsList[Dir.DOWN] = [
+                            i + 1, j]
+                        self.maze_template[i, j].wallsList[Dir.RIGHT] = [
+                            i, j + 1]
 
         self.maze_template[self.__entry_coordinates[0],
-                           self.__entry_coordinates[1]] = 0
+                           self.__entry_coordinates[1]].value = 0
 
     def get_maze_template(self):
         return self.maze_template
 
     def gen_maze(self):
-        pass
+        curr = self.__entry_coordinates
+        walls = []
+
+        for w in self.maze_template[curr[0], curr[1]].wallsList:
+            walls.append(w)
